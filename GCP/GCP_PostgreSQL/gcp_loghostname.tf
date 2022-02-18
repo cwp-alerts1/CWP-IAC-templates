4472a5c72f18c5324c35f3b3a984c6343f91a627
RadhaKrishna-Divakaruni <97581995+RadhaKrishna-Divakaruni@users.noreply.github.com>
2022-02-18 18:06:34 +0530

    Add files via upload

---
+++ GCP/GCP_PostgreSQL/gcp_loghostname.tf
@@ -0,0 +1,18 @@
+resource "google_sql_database_instance" "hostname" {
+  name             = "master-instance"
+  database_version = "POSTGRES_11"
+  region           = "us-central1"
+  settings {
+         database_flags {
+            name  = "log_hostname"
+            value = "TEST"
+          }
+         ip_configuration {
+             require_ssl  = "true"
+  }
+}
+}
+resource "google_sql_ssl_cert" "client_cert" {
+  common_name = "client-name"
+  instance    = google_sql_database_instance.default.name
+}
---
+++ GCP/GCP_PostgreSQL/gcp_logstatementstatus.tf
@@ -0,0 +1,18 @@
+resource "google_sql_database_instance" "statementstatus" {
+  name             = "master-instance"
+  database_version = "POSTGRES_11"
+  region           = "us-central1"
+  settings {
+         database_flags {
+            name  = "log_statement_status"
+            value = "on"
+          }
+         ip_configuration {
+             require_ssl  = "true"
+  }
+}
+}
+resource "google_sql_ssl_cert" "client_cert" {
+  common_name = "client-name"
+  instance    = google_sql_database_instance.default.name
+}
