e1e5798fd60aeea2cbd9420dd92170d0c2670cab
RadhaKrishna-Divakaruni <97581995+RadhaKrishna-Divakaruni@users.noreply.github.com>
2022-02-18 18:15:44 +0530

    Add files via upload

---
+++ GCP/GCP_SQLSERVER/gcp_sql_extscripts.tf
@@ -0,0 +1,16 @@
+resource "google_sql_database_instance" "ext scripts" {
+  name             = "master-instance"
+  database_version = "SQLSERVER_2017_STANDARD"
+  region           = "us-central1"
+  
+  settings {
+         database_flags {
+            name  = "external scripts enabled"
+            value = "on"
+          }
+         ip_configuration {
+             require_ssl  = "true"
+  }
+
+  }
+}
