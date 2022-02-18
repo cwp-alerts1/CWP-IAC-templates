4398a628f3f3caf62dac4f2f5c5db3fa82676b66
RadhaKrishna-Divakaruni <97581995+RadhaKrishna-Divakaruni@users.noreply.github.com>
2022-02-18 18:10:12 +0530

    Add files via upload

--- GCP/GCP_Compute/gcp_integrity.tf
+++ GCP/GCP_Compute/gcp_integrity.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 3.11"
-    }
-  }
-}
 resource "google_compute_instance" "integrity" {
   name         = "test"
   machine_type = "n1-standard-1"
--- GCP/GCP_Compute/gcp_ipforwarding.tf
+++ GCP/GCP_Compute/gcp_ipforwarding.tf
@@ -1,12 +1,3 @@
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
 resource "google_compute_instance" "ipforwarding" {
   name         = "test"
   machine_type = "n1-standard-1"
