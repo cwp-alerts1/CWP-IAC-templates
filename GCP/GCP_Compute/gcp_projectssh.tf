8de31c4bc6bdc50da012d0b961bcd3ca0f9d625b
cwp-alerts1 <103568629+cwp-alerts1@users.noreply.github.com>
2022-04-15 14:12:51 +0530

    Delete GCP directory

--- GCP/GCP_Compute/gcp_projectssh.tf
+++
@@ -1,8 +0,0 @@
-resource "google_compute_instance" "ssh" {
-  name         = "test"
-  machine_type = "n1-standard-1"
-  zone         = "us-central1-a"
-  metadata = {
-     block-project-ssh-keys = false
-  }
-}
