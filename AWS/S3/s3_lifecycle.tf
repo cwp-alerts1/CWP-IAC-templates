37070889c8de3f37be7b5f4813e173e8351a4087
cwp-alerts1 <103568629+cwp-alerts1@users.noreply.github.com>
2022-04-15 14:12:29 +0530

    Delete AWS directory

--- AWS/S3/s3_lifecycle.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "s3_lifecycle" {
-  bucket = "examplebuckettftest"
-  acl    = "public-read-write"
-  logging {
-    target_bucket = aws_s3_bucket.log_bucket.id
-    target_prefix = "log/"
-    }
-}
